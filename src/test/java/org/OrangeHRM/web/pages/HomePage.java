71364980c8296ea38d0d2c0d3f5a2af9c22dff3c
Sphelele <sphelelez101@gmail.com>
2024-01-22 15:35:03 +0200

    LoginPage Class added

--- src/test/java/org/OrangeHRM/web/pages/HomePage.java
+++ src/test/java/org/OrangeHRM/web/pages/HomePage.java
@@ -1,9 +1,27 @@
 package org.OrangeHRM.web.pages;
 
+import org.OrangeHRM.web.utils.WaitHelper;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage {
+
     private final WebDriver driver;
 
+    private final WaitHelper waitHelper;
 
+    @FindBy(id = "txtUsername")
+    @CacheLookup
+    WebElement usernameField;
+
+    @FindBy(id = "txtPassword")
+    @CacheLookup
+    WebElement passwordField;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        waitHelper = new WaitHelper(driver);
+    }
 }
