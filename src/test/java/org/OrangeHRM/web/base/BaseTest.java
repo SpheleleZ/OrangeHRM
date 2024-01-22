18a11f674d911795d1833833d9f06fda758fcb0c
Sphelele <sphelelez101@gmail.com>
2024-01-22 12:48:43 +0200

    Base Class Methods added Setp,OpenPage and tearDown

--- src/test/java/org/OrangeHRM/web/base/BaseTest.java
+++ src/test/java/org/OrangeHRM/web/base/BaseTest.java
@@ -1,5 +1,23 @@
 package org.OrangeHRM.web.base;
 
-public class BaseTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class BaseTest {
+    protected WebDriver driver;
+
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    public void openPage(String url){
+        driver.get(url);
+    }
+
+    public void tearDown(){
+        driver.quit();
+    }
 }
